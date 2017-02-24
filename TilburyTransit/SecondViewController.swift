52073804cfe2fc3eaf295ce43dfce9820f0dcfed
Dean Silfen <dean.silfen@gmail.com>
2017-02-24 00:00:59 -0500

    Initial Commit

---
+++ TilburyTransit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TilburyTransit
+//
+//  Created by Dean Silfen on 2/24/17.
+//  Copyright © 2017 Dean Silfen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
